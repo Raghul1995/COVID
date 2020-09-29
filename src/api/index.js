5e5ba9573e1169f1051fb9759ef13ddeb5590aeb
Raghul1995 <rahulmohan997@gmail.com>
2020-09-29 15:19:29 +0200

    CoVID tracker

---
+++ src/api/index.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) => {
+	let changeableUrl = url;
+
+	if (country) {
+		changeableUrl = `${url}/countries/${country}`;
+	}
+
+	try {
+		const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl); //destructuring the data to get individual componnets such as {confirmed, recovered, deaths, lastupdate}
+		return { confirmed, recovered, deaths, lastUpdate }; // getting the destructed data
+	} catch (err) {
+		return err;
+	}
+};
+
+export const fetchDailyData = async () => {
+	try {
+		const { data } = await axios.get(`${url}/daily`);
+		return data.map(({ confirmed, deaths, reportDate: date }) => ({
+			confirmed: confirmed.total,
+			deaths: deaths.total,
+			date
+		}));
+		//since dailyData is an array we convert the dailydata using the method mentioned
+	} catch (err) {
+		return err;
+	}
+};
+
+export const fetchCountries = async () => {
+	try {
+		const { data: { countries } } = await axios.get(`${url}/countries`);
+
+		return countries.map((country) => country.name);
+	} catch (err) {
+		return err;
+	}
+};
